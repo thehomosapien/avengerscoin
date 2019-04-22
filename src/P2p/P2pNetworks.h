4c37c5f2475277a9138d2431921b458c31389dc7
thehomosapien <rishabhshukla@opulasoft.com>
2019-04-22 15:02:51 +0530

    avengerscoin

---
+++ src/P2p/P2pNetworks.h
@@ -0,0 +1,14 @@
+// Copyright (c) 2011-2017 The Cryptonote developers
+// Copyright (c) 2014-2017 XDN developers
+// Copyright (c) 2016-2017 BXC developers
+// Copyright (c) 2017-2019 UltraNote developers 
+// Copyright (c) 2019 Avengers developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+#pragma once
+
+namespace CryptoNote
+{
+const static boost::uuids::uuid CRYPTONOTE_NETWORK = { { 0xA9, 0x61, 0xDF, 0xA3, 0xF7, 0xC9, 0x14, 0x38, 0xE7, 0x18, 0xA3, 0x87, 0xDF, 0xA5, 0x92, 0x37 } };
+}
